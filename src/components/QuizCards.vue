5e560527fce7135f22d04ff0df69a52bf6a87150
hasanaimroatun <imroatunhasana@gmail.com>
2022-11-22 12:35:59 +0700

    create and export file

---
+++ src/components/QuizCards.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <div>Country Quiz</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Quiz-card'
+    }
+</script>
+
+<style scoped>
+
+</style>
