678d78fc232e4ffb05a29852e0403149584afd1f
hasanaimroatun <imroatunhasana@gmail.com>
2022-11-23 07:05:59 +0700

    add elements and props also export file

---
+++ src/components/QuizResult.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <div class="card text-center mt-3" :style="{width: '464px', background: 'white'}">
+            <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <div class="btnContainer">
+                    Result
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Quiz-result',
+        props: ['activeTab']
+    }
+</script>
+
+<style scoped>
+
+</style>
