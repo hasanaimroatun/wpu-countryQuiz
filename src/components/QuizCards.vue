08ffec64b5ac74193eaba8665a895eeefae1e8f8
hasanaimroatun <imroatunhasana@gmail.com>
2022-11-23 07:03:18 +0700

    import QuizQuestions and QuizResult, add keep-alive element, components, and data

--- src/components/QuizCards.vue
+++ src/components/QuizCards.vue
@@ -2,30 +2,10 @@
     <div>
         <div class="position-absolute top-50 start-50 translate-middle">
             <div class="text-start" :style="{width: '464px'}">COUNTRY QUIZ</div>
-            <div class="card text-center mt-3" :style="{width: '464px', background: 'white'}">
-                <div class="card-body">
-                    <h5 class="card-title">Special title treatment</h5>
-                    <div class="btnContainer d-flex flex-column gap-4">
-                        <button type="button" class="btn btn-outline-secondary text-start">
-                            <span>A</span>
-                            <span class="me-3"></span>
-                        </button>
-                        <button type="button" class="btn btn-outline-secondary text-start">
-                            <span>B</span>
-                            <span class="me-3"></span>
-                        </button>
-                        <button type="button" class="btn btn-outline-secondary text-start">
-                            <span>C</span>
-                            <span class="me-3"></span>
-                        </button>
-                        <button type="button" class="btn btn-outline-secondary text-start">
-                            <span>D</span>
-                            <span class="me-3"></span>
-                        </button>
-
-                    </div>
-                </div>
-            </div>
+            
+            <keep-alive>
+                <component :is="activeTab" class="content" :activeTab='activeTab'/>
+            </keep-alive>
         </div>
         <div class="createdBy mb-4 position-absolute bottom-0 start-50 translate-middle-x">
             created by <a href="https://github.com/hasanaimroatun/wpu-countryQuiz">hasanaimroatun</a> - devChallenges.io
@@ -34,17 +14,24 @@
 </template>
 
 <script>
+import QuizQuestions from './QuizQuestions.vue'
+import QuizResult from './QuizResult.vue'
+
     export default {
         name: 'Quiz-card',
+        components: {
+            QuizQuestions,
+            QuizResult,
+        },
         data() {
             return {
-                
+               activeTab: 'QuizQuestions' 
             }
         }
     }
 </script>
 
-<style scoped>
+<style>
 @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@500;700&family=Poppins:wght@400;500;600;700&display=swap');
 
 .card-body {
