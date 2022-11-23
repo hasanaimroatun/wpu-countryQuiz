9d191b856abb0c80e75cec1593325863c431fa0c
hasanaimroatun <imroatunhasana@gmail.com>
2022-11-23 14:00:33 +0700

    add img, import Logo2 and add setup( )

--- src/components/QuizResult.vue
+++ src/components/QuizResult.vue
@@ -1,11 +1,12 @@
 <template>
     <div>
-        <div class="card text-center mt-3" :style="{width: '464px', background: 'white'}">
+        <div class="card text-center" :style="{width: '464px', background: 'white'}">
             <div class="card-body">
+                <img :src="Logo2" alt="logo2" id="logo2">
                 <h5 class="card-title">Results</h5>
                 <div class="btnContainer">
-                    <div>You got 4 correct answers</div>
-                    <button type="button" class="btn btn-outline-secondary" @click="$emit('showQuestions')">
+                    <div class="result">You got <span>4</span> correct answers</div>
+                    <button type="button" class="btn" @click="$emit('showQuestions')">
                         Try again
                     </button>
                 </div>
@@ -15,12 +16,65 @@
 </template>
 
 <script>
+import Logo2 from '@/assets/undraw_winners_ao2o 2.svg'
+
     export default {
         name: 'Quiz-result',
-        emits: ['showQuestions']
+        emits: ['showQuestions'],
+        setup() {
+            return {
+                Logo2
+            }
+        }
     }
 </script>
 
 <style scoped>
+.card-body {
+    padding: 49px 78px 33px;
+    font-family: 'Poppins', sans-serif;
+}
+
+#logo2 {
+    width: 238px;
+}
+
+.card-title {
+    font-weight: 700;
+    font-size: 48px;
+    line-height: 72px;
+    margin-top: 72px;
+    color: #1D355D;
+}
+
+.btnContainer {
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 54px;
+    color: #1D355D;
+    white-space: nowrap;
+}
+
+.btnContainer .result span {
+    font-weight: 700;
+    font-size: 36px;
+    color: #6FCF97;
+}
+
+.btn {
+    padding: 15px 61px;
+    margin-top: 71px;
+    font-weight: 600;
+    font-size: 18px;
+    line-height: 27px;
+    color: #1D355D;
+    border: 1px solid #1D355D;
+}
+
+.btn:hover,
+.btn:focus {
+    background-color: #1D355D;
+    color: white;
+}
 
 </style>
